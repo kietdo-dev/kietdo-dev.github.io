50601ad80bc7c199680656dc423fe4141a14256e
teq-kietdo <kietdo@teqnological.asia>
2025-03-18 16:40:44 +0700

    feat: initial

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   reactStrictMode: true,
+  basePath: "/",
 };
 
 export default nextConfig;
